16c0b2c8434388ef540e5dd094344cbe50ae7182
Omkar <omkarkore1431@gmail.com>
2023-09-03 23:50:00 +0530

    Weekly test

---
+++ Json/src/Main.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+
+            String apiUrl = "https://api.nationalize.io/?name=nathaniel";
+
+            URL url = new URL(apiUrl);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+
+                in.close();
+
+
+                System.out.println(response.toString());
+            } else {
+                System.out.println(responseCode);
+            }
+        } catch (Exception e) {
+
+        }
+    }
+}
